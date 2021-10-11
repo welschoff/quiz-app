67aa13295188af597f4229f8f99a6b8a15b8b339
welschoff <h.welschoff@gmx.de>
2021-10-11 17:18:56 +0200

    Add function

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-let myQuestion = document.querySelector("p");
+const correctAnswer = true;
+
+const myQuestion = document.querySelector(".question");
 myQuestion.textContent = "Do you like Javascript?";
+
+const noButton = document.querySelector(".no");
+noButton.onclick = function () {
+  if (correctAnswer === true) {
+    alert("This is correct");
+  } else {
+    alert("This is incorrect");
+  }
+};
+
+const yesButton = document.querySelector(".yes");
+yesButton.onclick = function () {
+  if (correctAnswer === false) {
+    alert("This is correct");
+  } else {
+    alert("This is incorrect");
+  }
+};
